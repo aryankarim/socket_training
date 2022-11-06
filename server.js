9f66d5c1c2d0c77d34f2c701c5cc55c2ae2c1e40
AryanKarim <aryanmajid97@gmail.com>
2022-11-06 19:29:49 +0300

    setup: add socket.io and nodemon

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hi')
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+const server = app.listen(PORT);
+
+app.use(express.static("public"));
+
+console.log("Server running on port " + PORT);
